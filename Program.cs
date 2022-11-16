9dfa8301756c3e5d1a254d48b8c4e39af50352ae
Beazej <beata-alex@wp.pl>
2022-11-16 13:39:54 +0100

    Added SQLInterpreter function

--- Program.cs
+++ Program.cs
@@ -26,13 +26,32 @@
            List<Object> list= new List<Object>();
             return list;
         }
-        static List<Object> Where(List<Object> list, string query)
+        static List<Object> Where(List<Object> list, string[] query)
         {
             return list;
         }
-        static List<Object> Select(List<Object> list, string query)
+        static List<Object> Select(List<Object> list, string[] query)
         {
             return list;
+        }
+        static string SQLInterpreter(Data data, string query)
+        {
+            string[] elements = query.Split(' ');
+            int ids=0;
+            for(int i=0; i<elements.Length; i++)
+            {
+                if (elements[i].Equals("select"))
+                {
+                    ids = i;
+                }
+            }
+            string fromquery = elements[1];
+            string[] wherequery = new string[ids - 3];
+            Array.Copy(elements, 3, wherequery, 0, ids - 3);
+            string[]selectquery = new string[elements.Length - ids - 1];
+            Array.Copy(elements, ids + 1, wherequery, 0, elements.Length - ids - 1);
+            List<Object> output = Select(Where(From(data, fromquery), wherequery), selectquery);
+            return output.ToString() ?? "null";
         }
         static void Main(string[] args)
         {
