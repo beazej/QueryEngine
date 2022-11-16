7d91ebafa75572c1148daf0b56eeebf6b5d17faa
Beazej <beata-alex@wp.pl>
2022-11-16 15:25:38 +0100

    Added constructors and example data

--- Program.cs
+++ Program.cs
@@ -1,29 +1,47 @@
-﻿namespace QueryEngine
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QueryEngine
 {
     public class Data
     {
         public List<User> Users;
         public List<Order> Orders;
+        public Data(List<User> users, List<Order> orders)
+        {
+            Users = users;
+            Orders = orders;
+        }   
     }
 
     public class User
     {
-        public string Email; 
-        public string FullName;
-        public int Age;
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public User(string email, string fullName, int age)
+        {
+            Email = email;
+            FullName = fullName;
+            Age = age;
+        }
     }
 
     public class Order
     {
         public int Id;
         public string Name;
+        public Order(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 
     internal class Program
     {
         static List<Object> From(Data data, string query)
         {
-           List<Object> list= new List<Object>();
+            List<Object> list= new List<Object>();
             return list;
         }
         static List<Object> Where(List<Object> list, string[] query)
@@ -48,14 +66,20 @@
             string fromquery = elements[1];
             string[] wherequery = new string[ids - 3];
             Array.Copy(elements, 3, wherequery, 0, ids - 3);
-            string[]selectquery = new string[elements.Length - ids - 1];
-            Array.Copy(elements, ids + 1, wherequery, 0, elements.Length - ids - 1);
+            string[] selectquery = new string[elements.Length - ids - 1];
+            Array.Copy(elements, ids + 1, selectquery, 0, elements.Length - ids - 1);
             List<Object> output = Select(Where(From(data, fromquery), wherequery), selectquery);
             return output.ToString() ?? "null";
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Query Engine");
+            List<User> users = new List<User>();
+            users.Add(new User("email", "name", 40));
+            List<Order> orders = new List<Order>();
+            orders.Add(new Order(0, "a"));
+            Data data = new Data(users, orders);
+            string query = "from Users where age=40 select email";
+            Console.WriteLine(SQLInterpreter(data, query));
         }
     }
 }
