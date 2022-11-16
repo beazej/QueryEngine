d521d7aad2188f1813a079122957f8c236a9c878
Beazej <beata-alex@wp.pl>
2022-11-16 12:36:18 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace QueryEngine
+{
+    public class Data
+    {
+        public List<User> Users;
+        public List<Order> Orders;
+    }
+
+    public class User
+    {
+        public string Email; 
+        public string FullName;
+        public int Age;
+    }
+
+    public class Order
+    {
+        public int Id;
+        public string Name;
+    }
+
+    internal class Program
+    {
+        static List<Object> From(Data data, string query)
+        {
+           List<Object> list= new List<Object>();
+            return list;
+        }
+        static List<Object> Where(List<Object> list, string query)
+        {
+            return list;
+        }
+        static List<Object> Select(List<Object> list, string query)
+        {
+            return list;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Query Engine");
+        }
+    }
+}
